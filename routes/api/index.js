a5e3fa5f040951e826e2acc74de86d866a451293
ndoppler <112019993+ndoppler@users.noreply.github.com>
2024-10-09 21:39:24 -0500

    added thought controller and routes - apis should be fully functional

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-//const userRoutes = require('./userRoutes');
-//const thoughtRoutes = require('./thoughtRoutes');
+// Import route files
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
 
-//router.use('/users', userRoutes);
-//router.use('/thoughts', thoughtRoutes);
-
+// Use routes
+router.use('/users', userRoutes);
+router.use('/posts', thoughtRoutes);
 
 module.exports = router;
